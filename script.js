e7d64d56b0e0191579c9d7fa1deba7e647e4e7d1
AhmedSajid-786 <ahmedsajid782020@gmail.com>
2024-10-04 19:02:48 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let o = {
+    "name" : "Ahmed Sajid",
+    "Job code" : "7700",
+};
+
+console.log(o);  // Logs the object without salary
+
+o.salary = "755000";  // Adds the salary property
+
+console.log(o);  // Logs the object with salary added
